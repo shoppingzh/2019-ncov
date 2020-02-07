16def2bea1aab78111b1a0d5e0fd1de278b6dc93
shoppingzh <1049262057@qq.com>
2020-02-07 16:24:05 +0800

    1. 提取公共组件 2. 增加世界疫情地图 3. 增加每日增长数预览

---
+++ client/src/components/WorldMap/index.vue
@@ -0,0 +1,132 @@
+<template>
+  <div
+    ref="map"
+    :style="{width: width, height: height}">
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import worldGeo from 'echarts/map/json/world.json'
+import nameMap from '@/assets/worldNameMap.js'
+
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    },
+    data: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      map: null
+    }
+  },
+  computed: {
+    mapData () {
+      if (this.data) {
+        const data = []
+        this.data.forEach((obj) => {
+          data.push({
+            name: obj.name,
+            value: parseInt(obj.value),
+            sus: parseInt(obj.susNum),
+            cure: parseInt(obj.cureNum),
+            death: parseInt(obj.deathNum)
+          })
+        })
+        return data
+      }
+      return []
+    }
+  },
+  watch: {
+    mapData (newVal, oldVal) {
+      this.render()
+    }
+  },
+  mounted () {
+    echarts.registerMap('world', worldGeo)
+    this.map = echarts.init(this.$refs.map)
+    this.map.setOption({
+      tooltip: {
+        show: true,
+        backgroundColor: 'rgba(255, 255, 255, .85)',
+        borderColor: '#ddd',
+        borderWidth: 1,
+        textStyle: {
+          color: '#333'
+        },
+        padding: 10,
+        formatter: (params, tickert, callback) => {
+          const data = params.data
+          if (data) {
+            let html = `
+              <b>${params.name}</b><br>
+              确诊: ${data.value}<br>
+              治愈: ${data.cure}<br>
+              死亡: ${data.death}
+            `
+            return html
+          }
+          return `<b>${params.name}</b><br> 暂无数据`
+        }
+      },
+      visualMap: {
+        type: 'piecewise',
+        showLabel: true,
+        pieces: [
+          { gte: 1, lte: 9 },
+          { gte: 10, lte: 99 },
+          { gte: 100, lte: 499 },
+          { gte: 500, lte: 1000 },
+          { gt: 1000 }
+        ],
+        left: '10px',
+        bottom: '30%',
+        padding: 0,
+        textStyle: {
+          fontSize: 10
+        },
+        itemWidth: 10,
+        itemHeight: 10,
+        itemGap: 5
+      },
+      series: [
+        {
+          type: 'map',
+          map: 'world',
+          zoom: 1.1,
+          data: this.mapData,
+          nameMap: nameMap
+        }
+      ]
+    })
+  },
+  methods: {
+    render () {
+      this.map.setOption({
+        series: [
+          {
+            data: this.mapData
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/index.vue
+++ client/src/views/index.vue
@@ -7,20 +7,24 @@
       height="200px">
     </van-image>
     <div class="container-y">
-      <van-row type="flex" class="summary">
-        <van-col span="6">
+      <van-row type="flex">
+        <van-col span="6" class="summary">
+          <div v-if="data.add_daily" class="c-red add">+{{data.add_daily.addcon}}</div>
           <div class="c-red">{{ data.gntotal || '-' }}</div>
           <div class="label">确诊</div>
         </van-col>
-        <van-col span="6">
+        <van-col span="6" class="summary">
+          <div v-if="data.add_daily" class="c-orange add">+{{data.add_daily.addsus}}</div>
           <div class="c-orange">{{ data.sustotal || '-' }}</div>
           <div class="label">疑似</div>
         </van-col>
-        <van-col span="6">
+        <van-col span="6" class="summary">
+          <div v-if="data.add_daily" class="c-gray add">+{{data.add_daily.adddeath}}</div>
           <div class="c-gray">{{ data.deathtotal || '-' }}</div>
           <div class="label">死亡</div>
         </van-col>
-        <van-col span="6">
+        <van-col span="6" class="summary">
+          <div v-if="data.add_daily" class="c-green add">+{{data.add_daily.addcure}}</div>
           <div class="c-green">{{ data.curetotal || '-' }}</div>
           <div class="label">治愈</div>
         </van-col>
@@ -31,125 +35,51 @@
         <van-row type="flex">
           <div class="container-xy c-blue">疫情地图</div>
         </van-row>
-        <div
-          ref="chinaMap"
-          style="width: 100%; height: 300px;">
-        </div>
+        <china-map
+          :data="this.data.list">
+        </china-map>
+      </div>
+      <div class="card">
+        <van-row type="flex">
+          <div class="container-xy c-blue">世界疫情地图</div>
+        </van-row>
+        <world-map :data="this.data.worldlist">
+        </world-map>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import echarts from 'echarts'
-import chinaGeo from 'echarts/map/json/china.json'
 import logoSrc from '@/assets/logo.png'
 import jsonp from 'jsonp'
+import ChinaMap from '@/components/ChinaMap'
+import WorldMap from '@/components/WorldMap'
 
 export default {
+  components: {
+    ChinaMap,
+    WorldMap
+  },
   data () {
     return {
-      chinaMap: null,
       data: {},
       logoSrc: logoSrc
     }
   },
-  watch: {
-    data (newVal, oldVal) {
-      this.renderChart()
-    }
-  },
   mounted () {
-    // 初始化图形
-    echarts.registerMap('china', chinaGeo)
-    this.chinaMap = echarts.init(this.$refs.chinaMap)
-    this.chinaMap.setOption({
-      tooltip: {
-        show: true,
-        backgroundColor: 'rgba(255, 255, 255, .85)',
-        borderColor: '#ddd',
-        borderWidth: 1,
-        textStyle: {
-          color: '#333'
-        },
-        padding: 10,
-        formatter: (params, tickert, callback) => {
-          const data = params.data
-          if (data) {
-            let html = `
-              <b>${params.name}</b><br>
-              确诊: ${data.value}<br>
-              治愈: ${data.cure}<br>
-              死亡: ${data.death}
-            `
-            return html
-          }
-          return `<b>${params.name}</b><br> 暂无数据`
-        }
-      },
-      visualMap: {
-        type: 'piecewise',
-        showLabel: true,
-        pieces: [
-          { value: 0 },
-          { gte: 1, lte: 9 },
-          { gte: 10, lte: 99 },
-          { gte: 100, lte: 499 },
-          { gte: 500, lte: 1000 },
-          { gt: 1000 }
-        ],
-        right: '10px',
-        bottom: '30%',
-        padding: 0,
-        textStyle: {
-          fontSize: 10
-        },
-        itemWidth: 10,
-        itemHeight: 10,
-        itemGap: 5
-      }
-    })
-    // 获取数据(jsonp)
     jsonp('https://interface.sina.cn/news/wap/fymap2020_data.d.json', {}, (err, data) => {
       if (!err) {
         this.data = data.data
       }
     })
-  },
-  methods: {
-    renderChart () {
-      // 中国地图
-      const chinaMapData = []
-      this.data.list.forEach((obj) => {
-        chinaMapData.push({
-          name: obj.name,
-          value: parseInt(obj.value),
-          sus: parseInt(obj.susNum),
-          cure: parseInt(obj.cureNum),
-          death: parseInt(obj.deathNum)
-        })
-      })
-      this.chinaMap.setOption({
-        series: [
-          {
-            type: 'map',
-            map: 'china',
-            zoom: 1.1,
-            label: {
-              show: true,
-              fontSize: 8
-            },
-            data: chinaMapData
-          }
-        ]
-      })
-    }
   }
 }
 </script>
 
 <style scoped>
-  .summary{ text-align: center; }
   .card{ border-radius: 5px; margin-bottom: 15px; }
-  .label{ font-size: 12px;  color: #444; }
+  .summary{ text-align: center; }
+  .summary .add{ font-size: 12px; padding: 3px 0; opacity: .85; }
+  .summary .label{ font-size: 12px;  color: #666; padding-top: 2px; }
 </style>
